d5363e0b23f625935651569814d2651bb391cfb6
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-09-26 03:41:55 +0200

    Add image list component

---
+++ src/components/ImageList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "./ImageList.css";
+
+import ImageCard from './ImageCard';
+
+const ImageList = (props) => {
+  const images = props.photos.map((image) => {
+    return <ImageCard key={image.id} image={image}/>
+  });
+
+  return <div className="image-list">{images}</div>
+};
+
+export default ImageList;
