65f1b97d7858176d100e9ef27b9439dfefe68334
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-09-26 03:42:04 +0200

    Add app file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
+
+import unsplash from './utils/api';
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = { photos: [] };
+  }
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term }
+    }).catch(err => console.log(err));
+
+    this.setState({ photos: response.data.results});
+  }
+
+  render(){
+    return(
+      <div className="ui container mt-2" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <ImageList photos={this.state.photos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
